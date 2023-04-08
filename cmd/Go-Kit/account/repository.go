f93e270a0244d5e40088e8e52f1eadb8c465b501
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-08 13:53:42 -0500

    Create user repository method

--- cmd/Go-Kit/account/repository.go
+++ cmd/Go-Kit/account/repository.go
@@ -17,8 +17,16 @@ type repository struct {
 }
 
 func (r *repository) CreateUser(ctx context.Context, user User) error {
-	//TODO implement me
-	panic("implement me")
+	sql := `INSERT INTO users (id, email, password)
+			VALUES ($1, $2,$3)`
+	if user.Email == "" || user.Password == "" {
+		return RepositoryError
+	}
+	_, err := r.db.ExecContext(ctx, sql, user.Id, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *repository) GetUser(ctx context.Context, id string) (string, error) {
