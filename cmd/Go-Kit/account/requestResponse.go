62135d806bd654563d85f27e8bcbbb69dabb1684
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-08 14:43:28 -0500

    Decode and encode functions for create user

--- cmd/Go-Kit/account/requestResponse.go
+++ cmd/Go-Kit/account/requestResponse.go
@@ -1,5 +1,11 @@
 package account
 
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
 type (
 	CreateUserRequest struct {
 		Email    string `json:"email"`
@@ -15,3 +21,16 @@ type (
 		Email string `json:"email"`
 	}
 )
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+func decodeUserRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var req CreateUserRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
