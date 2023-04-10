6b150e4c2f13d876726719250c351cf5c11972b6
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-10 11:07:45 -0500

    Server and errors goroutines

--- cmd/Go-Kit/main.go
+++ cmd/Go-Kit/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"flag"
+	"fmt"
+	"github.com/Erickype/Go-Kit/account"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	_ "github.com/lib/pq"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
-const dbSource = "postgres://GoKit:GoKit@db/GoKit?sslmode=disable"
+const dbSource = "postgres://gokit:gokit@:5432/GoKit?sslmode=disable"
 
 func main() {
-	var _ = flag.String("http", "8080", "http listen address")
+	var httpAddress = flag.String("http", ":8080", "http listen address")
 	var logger log.Logger
 	{
 		logger = log.NewLogfmtLogger(os.Stderr)
@@ -25,9 +31,7 @@ func main() {
 	}
 
 	_ = level.Info(logger).Log("msg", "service started")
-	defer func(logger log.Logger, message ...interface{}) {
-		_ = logger.Log(message)
-	}(level.Info(logger), "msg", "service ended")
+	defer level.Info(logger).Log("msg", "service ended")
 
 	var db *sql.DB
 	{
@@ -38,9 +42,32 @@ func main() {
 			_ = level.Error(logger).Log("exit", err)
 			os.Exit(-1)
 		}
-		err = db.Close()
-		if err != nil {
-			return
-		}
 	}
+
+	flag.Parse()
+	ctx := context.Background()
+
+	var srv account.Service
+	{
+		repository := account.NewRepository(db, logger)
+		srv = account.NewService(repository, logger)
+	}
+
+	errs := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%c", <-c)
+	}()
+
+	endpoints := account.MakeEndpoints(srv)
+
+	go func() {
+		fmt.Println("listening on port", *httpAddress)
+		handler := account.NewHttpServer(ctx, endpoints)
+		errs <- http.ListenAndServe(*httpAddress, handler)
+	}()
+
+	level.Error(logger).Log("exit", <-errs)
 }
